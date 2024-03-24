b67910221eb0c1ad12fc9db983c47ff6df1b6bf3
Krintox <shashanksuggala@gmail.com>
2024-03-25 00:20:34 +0530

    fix: added CORS url

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://certgen-frontend.vercel.app'];
+const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://certgen-frontend.vercel.app', 'certgen-frontend.vercel.app'];
 app.use(cors({
   credentials: true,
   origin: allowedOrigins,
