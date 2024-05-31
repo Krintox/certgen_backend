2036675cdd9a58d6ca17ff83dfa991134aa7b4ff
Krintox <shashanksuggala@gmail.com>
2024-05-31 11:58:32 +0530

    fix: cross site request

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,31 +20,34 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const userDoc = await User.findOne({ username });
-      if (!userDoc) {
-        return res.status(400).json({ message: 'User not found' });
-      }
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        // logged in
-        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-          if (err) throw err;
-          res.cookie('token', token).json({
-            id: userDoc._id,
-            username,
-          });
-        });
-      } else {
-        res.status(400).json('Wrong credentials');
-      }
-    } catch (error) {
-      console.error('Error in login:', error);
-      res.status(500).json({ message: 'Internal server error' });
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.findOne({ username });
+    if (!userDoc) {
+      return res.status(400).json({ message: 'User not found' });
     }
-  };
-  
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: true, // Ensure this is set to true if using https
+          sameSite: 'None',
+        }).json({
+          id: userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('Wrong credentials');
+    }
+  } catch (error) {
+    console.error('Error in login:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 exports.profile = (req, res) => {
   const { token } = req.cookies;
@@ -60,5 +63,9 @@ exports.profile = (req, res) => {
 };
 
 exports.logout = (req, res) => {
-  res.cookie('token', '').json('ok');
+  res.cookie('token', '', {
+    httpOnly: true,
+    secure: true, // Ensure this is set to true if using https
+    sameSite: 'None',
+  }).json('ok');
 };
